237cb0c65a9ee2a9d99033e43cd40e1da4a1c3c0
Charles Max Wood <chuck@teachmetocode.com>
2011-01-06 13:15:14 -0700

    basic rails + devise + testing stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Delicious::Application.config.secret_token = '891085d5137a8e7c7a0254a8bf8581428d5fb6ed64164c7c8a32a5c1aed4df1d92f96b6d98a817695decd8134ba3723539b17e4775b700033d091916e85fb3aa'
